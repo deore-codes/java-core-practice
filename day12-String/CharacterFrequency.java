519ce140efca19863b2ea588974f7aac7e512263
Sandesh P Deore <deore.sandesh1@gmail.com>
2026-08-01 22:57:09 +0530

    Add program to count frequency of each character in string

---
+++ day12-String/CharacterFrequency.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class CharacterFrequency
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+
+        str = str.toLowerCase();
+
+        boolean visited[] = new boolean[str.length()];
+
+        System.out.println("\nCharacter Frequency:");
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(visited[i] || str.charAt(i) == ' ')
+            {
+                continue;
+            }
+
+            int count = 1;
+
+            for(int j = i + 1; j < str.length(); j++)
+            {
+                if(str.charAt(i) == str.charAt(j))
+                {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(str.charAt(i) + " -> " + count);
+        }
+
+        sc.close();
+    }
+}
