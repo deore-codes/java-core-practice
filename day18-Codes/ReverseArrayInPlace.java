cb6e106331cffa637539a75016a19b45f15e32d3
Sandesh P Deore <deore.sandesh1@gmail.com>
2026-08-17 23:33:17 +0530

    Implement in-place array reversal

---
+++ day18-Codes/ReverseArrayInPlace.java
@@ -0,0 +1,54 @@
+/*
+ * Program          : Reverse Array In Place
+ * Description      : Reverses an array without using an additional array.
+ * Time Complexity  : O(n)
+ * Space Complexity : O(1)
+ * Author           : Sandesh Deore
+ */
+
+import java.util.Scanner;
+
+public class ReverseArrayInPlace {
+
+    static void reverse(int[] arr) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        reverse(arr);
+
+        System.out.println("Reversed array:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
